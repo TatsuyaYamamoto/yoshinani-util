7bfbc46b3188a28d6377ed0a3f43c2dfbb5be37f
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-07-24 23:02:52 +0900

    fix, remove unnecessary throwing exception

--- src/main/java/net/sokontokoro_factory/yoshinani/file/config/Config.java
+++ src/main/java/net/sokontokoro_factory/yoshinani/file/config/Config.java
@@ -1,9 +1,7 @@
 package net.sokontokoro_factory.yoshinani.file.config;
 
-import java.io.IOException;
-
 public interface Config {
     String getFileName();
-    String getString(String key) throws IOException;
-    int getInt(String key) throws IOException;
+    String getString(String key);
+    int getInt(String key);
 }
